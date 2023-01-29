04d1d516ba0579dcc63ef3f059a875697c582d8e
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2023-01-29 13:14:30 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  window.navigator.serviceWorker.register('./sw.js', {
+    scope: __uv$config.prefix
+  }).then(() => {
+    let url = input.value.trim();
+    if (!isUrl(url)) url = 'https://google.com/search?q=' + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+var urll = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
+        if (urll) {
+            var win; {
+                if (win) { win.focus(); } else {
+                    win = window.open();
+                    win.document.body.style.margin = '0';
+                    win.document.body.style.height = '100vh';
+                    var iframe = win.document.createElement('iframe');
+                    iframe.style.border = 'none';
+                    iframe.style.width = '100%';
+                    iframe.style.height = '100%';
+                    iframe.style.margin = '0';
+                    iframe.src = urll;
+                    win.document.body.appendChild(iframe)
+                }
+            }
+        }
+    });
+
+});
+
+function isUrl(val = '') {
+  if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+  return false;
+};
