00f300870d1c97b86efa7b9c81932c65c38e2e7f
aereal98 <121365806+aereal98@users.noreply.github.com>
2023-02-01 17:11:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,127 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+import DocumentHook from './dom/document.js';
+import ElementApi from './dom/element.js';
+import NodeApi from './dom/node.js';
+import AttrApi from './dom/attr.js';
+import FunctionHook from './native/function.js';
+import ObjectHook from './native/object.js';
+import Fetch from './requests/fetch.js';
+import Xhr from './requests/xhr.js';
+import EventSourceApi from './requests/eventsource.js';
+import History from './history.js';
+import LocationApi from './location.js';
+import MessageApi from './message.js';
+import NavigatorApi from './navigator.js';
+import Workers from './worker.js';
+import URLApi from './url.js';
+import EventEmitter from 'events';
+import StorageApi from './storage.js';
+import StyleApi from './dom/style.js';
 
-form.addEventListener('submit', async event => {
-  event.preventDefault();
-  window.navigator.serviceWorker.register('./sw.js', {
-    scope: __uv$config.prefix
-  }).then(() => {
-    let url = input.value.trim();
-    if (!isUrl(url)) url = 'https://google.com/search?q=' + url;
-    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+class UVClient extends EventEmitter {
+    constructor(window = self, worker = !window.window) {
+        super();
+        /**
+         * @type {typeof self}
+         */
+        this.window = window;
+        this.nativeMethods = {
+            fnToString: this.window.Function.prototype.toString,
+            defineProperty: this.window.Object.defineProperty,
+            getOwnPropertyDescriptor:
+                this.window.Object.getOwnPropertyDescriptor,
+            getOwnPropertyDescriptors:
+                this.window.Object.getOwnPropertyDescriptors,
+            getOwnPropertyNames: this.window.Object.getOwnPropertyNames,
+            keys: this.window.Object.keys,
+            getOwnPropertySymbols: this.window.Object.getOwnPropertySymbols,
+            isArray: this.window.Array.isArray,
+            setPrototypeOf: this.window.Object.setPrototypeOf,
+            isExtensible: this.window.Object.isExtensible,
+            Map: this.window.Map,
+            Proxy: this.window.Proxy,
+        };
+        this.worker = worker;
+        this.fetch = new Fetch(this);
+        this.xhr = new Xhr(this);
+        this.history = new History(this);
+        this.element = new ElementApi(this);
+        this.node = new NodeApi(this);
+        this.document = new DocumentHook(this);
+        this.function = new FunctionHook(this);
+        this.object = new ObjectHook(this);
+        this.message = new MessageApi(this);
+        this.navigator = new NavigatorApi(this);
+        this.eventSource = new EventSourceApi(this);
+        this.attribute = new AttrApi(this);
+        this.url = new URLApi(this);
+        this.workers = new Workers(this);
+        this.location = new LocationApi(this);
+        this.storage = new StorageApi(this);
+        this.style = new StyleApi(this);
+    }
+    initLocation(rewriteUrl, sourceUrl) {
+        this.location = new LocationApi(
+            this,
+            sourceUrl,
+            rewriteUrl,
+            this.worker
+        );
+    }
+    override(obj, prop, wrapper, construct) {
+        // if (!(prop in obj)) return false;
+        const wrapped = this.wrap(obj, prop, wrapper, construct);
+        return (obj[prop] = wrapped);
+    }
+    overrideDescriptor(obj, prop, wrapObj = {}) {
+        const wrapped = this.wrapDescriptor(obj, prop, wrapObj);
+        if (!wrapped) return {};
+        this.nativeMethods.defineProperty(obj, prop, wrapped);
+        return wrapped;
+    }
+    wrap(obj, prop, wrap, construct) {
+        const fn = obj[prop];
+        if (!fn) return fn;
+        const wrapped =
+            'prototype' in fn
+                ? function attach() {
+                      return wrap(fn, this, [...arguments]);
+                  }
+                : {
+                      attach() {
+                          return wrap(fn, this, [...arguments]);
+                      },
+                  }.attach;
 
-var urll = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
-        if (urll) {
-            var win; {
-                if (win) { win.focus(); } else {
-                    win = window.open();
-                    win.document.body.style.margin = '0';
-                    win.document.body.style.height = '100vh';
-                    var iframe = win.document.createElement('iframe');
-                    iframe.style.border = 'none';
-                    iframe.style.width = '100%';
-                    iframe.style.height = '100%';
-                    iframe.style.margin = '0';
-                    iframe.src = urll;
-                    win.document.body.appendChild(iframe)
+        if (construct) {
+            wrapped.prototype = fn.prototype;
+            wrapped.prototype.constructor = wrapped;
+        }
+
+        this.emit('wrap', fn, wrapped, !!construct);
+
+        return wrapped;
+    }
+    wrapDescriptor(obj, prop, wrapObj = {}) {
+        const descriptor = this.nativeMethods.getOwnPropertyDescriptor(
+            obj,
+            prop
+        );
+        if (!descriptor) return false;
+        for (let key in wrapObj) {
+            if (key in descriptor) {
+                if (key === 'get' || key === 'set') {
+                    descriptor[key] = this.wrap(descriptor, key, wrapObj[key]);
+                } else {
+                    descriptor[key] =
+                        typeof wrapObj[key] == 'function'
+                            ? wrapObj[key](descriptor[key])
+                            : wrapObj[key];
                 }
             }
         }
-    });
+        return descriptor;
+    }
+}
 
-});
-
-function isUrl(val = '') {
-  if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-  return false;
-};
-
+export default UVClient;
+if (typeof self === 'object') self.UVClient = UVClient;
